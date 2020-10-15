4cafd51d03ebededbb9e811b357cfeac191664b0
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-15 17:13:05 -0300

    File to control and treat project errors :hammer:

---
+++ src/errors/handler.ts
@@ -0,0 +1,20 @@
+import { ErrorRequestHandler } from "express";
+import { ValidationError } from "yup";
+
+interface ValidationErrors {
+    [key: string]: string[];
+}
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+    console.error(error);
+    if (error instanceof ValidationError) {
+        let errors: ValidationErrors = {};
+        error.inner.forEach((err) => {
+            errors[err.path] = err.errors;
+        });
+
+        return res.status(400).json({ message: "Validation fails", errors });
+    }
+    return res.status(500).json({ message: "Internal Server Error " });
+};
+
+export default errorHandler;
